3857182e709fec11af592c3055095cfe38b313c1
VictorSabater <victor.sabatergarcia@cieep.com>
2024-01-17 19:38:44 +0100

    version 5.2 probar tabs y comentarios

--- ionic/frontendI/src/app/pages/tabs/tabs-routing.module.ts
+++ ionic/frontendI/src/app/pages/tabs/tabs-routing.module.ts
@@ -6,26 +6,7 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirectTo: 'Deportes',
-        pathMatch: "full"
-      },
-      {
-        path: 'Deportes',
-        loadChildren: () => import('../secciones/secciones.module').then(m => m.SeccionesPageModule)
-      },
-      {
-        path: 'Videojuegos',
-        loadChildren: () => import('../secciones/secciones.module').then(m => m.SeccionesPageModule)
-      },
-      {
-        path: 'Lectura',
-        loadChildren: () => import('../secciones/secciones.module').then(m => m.SeccionesPageModule)
-      }
-    ]
+    component: TabsPage
   }
 ];
 
